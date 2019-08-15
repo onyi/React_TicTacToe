0a716fd88f397c5c2fc669995ffb604b9889f678
Onyi Cho <onyicho@gmail.com>
2019-08-15 15:20:18 -0700

    Add css for layout, and import normalize.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
+
 
 import Game from './components/game'
 
