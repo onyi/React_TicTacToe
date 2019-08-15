c284b5f28392a9c887337cac5286eded8a3439ce
Onyi Cho <onyicho@gmail.com>
2019-08-15 16:53:50 -0700

    Remove comments

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -21,25 +21,16 @@ class Board extends React.Component {
             {this.createGameBoardGrid(0, squares[0], placeMove)}
             {this.createGameBoardGrid(1, squares[1], placeMove)}
             {this.createGameBoardGrid(2, squares[2], placeMove)}
-            {/* <Square value={squares[1]} placeMove={placeMove}></Square>
-            <Square value={squares[2]} placeMove={placeMove}></Square>
-            <Square value={squares[3]} placeMove={placeMove}></Square> */}
           </div>
           <div className="game-board-row">
             {this.createGameBoardGrid(3, squares[3], placeMove)}
             {this.createGameBoardGrid(4, squares[4], placeMove)}
             {this.createGameBoardGrid(5, squares[5], placeMove)}
-            {/* <Square value={squares[4]} placeMove={placeMove}></Square>
-            <Square value={squares[5]} placeMove={placeMove}></Square>
-            <Square value={squares[6]} placeMove={placeMove}></Square> */}
           </div>
           <div className="game-board-row">
             {this.createGameBoardGrid(6, squares[6], placeMove)}
             {this.createGameBoardGrid(7, squares[7], placeMove)}
             {this.createGameBoardGrid(8, squares[8], placeMove)}
-            {/* <Square value={squares[7]} placeMove={placeMove}></Square>
-            <Square value={squares[8]} placeMove={placeMove}></Square>
-            <Square value={squares[9]} placeMove={placeMove}></Square> */}
           </div>
 
         </div>
